6e614d6cc8d465d262b0c910a6a63466fd457933
Venoim <ascholz47@gmail.com>
2024-02-02 19:24:15 +0100

    create relation Ligin front with backend

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { connectToDB, connection } from "../conectDB.js";
+// import bcrypt from "bcrypt";
 
 const router = express.Router();
 
@@ -32,6 +33,7 @@ router.get("/:id", async (req, res) => {
       name: item[1],
       surname: item[2],
       email: item[3],
+      password: item[4], //do usuniecia
     }));
 
     if (sql) {
@@ -50,10 +52,13 @@ router.get("/:id", async (req, res) => {
 router.post("/", async (req, res) => {
   const { name, surname, email, password } = req.body;
 
-  if (!name || !surname || !email) {
+  if (!name || !surname || !email || !password) {
     return res.status(400).json({ error: "Brak wymaganych danych" });
   }
   try {
+    //zabzpieczenie hasla przed zapisem do BD
+    // const hashedPassword = await bcrypt.hash(password, 10);
+
     await connection.query(
       `INSERT INTO public.${from} (name, surname, email, password) VALUES ('${name}', '${surname}', '${email}', '${password}')`
     );
@@ -68,14 +73,61 @@ router.post("/check-email", async (req, res) => {
   try {
     const { email } = req.body;
 
-    // Sprawdź, czy email już istnieje w bazie danych
     const result = await connection.query(
       `SELECT * FROM public.${from} WHERE email = '${email}'`
     );
 
-    res.json({ exists: result.rows.length > 0 }); // Zwróć wynik sprawdzenia (true/false)
+    res.json({ exists: result.rows.length > 0 });
   } catch (error) {
     console.error("Błąd podczas sprawdzania emaila:", error);
+    res.status(500).json({ error: "Błąd serwera", details: error.message });
+  }
+});
+// logowanie użytkownika
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ error: "Brak wymaganych danych logowania" });
+  }
+
+  try {
+    const result = await connection.query(
+      `SELECT * FROM public.${from} WHERE email = '${email}'`
+    );
+
+    if (result.rows.length > 0) {
+      const user = {
+        password: result.rows[0][4],
+      };
+      if (user.password) {
+        // Sprawdź hasło
+        console.log("Przesłane dane:", email, password);
+        console.log("Zapisane hasło w bazie danych:", user.password);
+
+        const isPasswordValid = user.password === password;
+
+        if (isPasswordValid) {
+          // Hasła są identyczne
+          const { password, ...userDataWithoutPassword } = user;
+          res.json(userDataWithoutPassword);
+        } else {
+          // Hasła się nie zgadzają
+          res.status(401).json({ error: "Błędne dane logowania" });
+        }
+      } else {
+        // Użytkownik istnieje, ale brak hasła w bazie danych
+        res.status(500).json({
+          error: "Błąd serwera",
+          details: "Brak hasła w bazie danych",
+        });
+      }
+    } else {
+      // Użytkownik nie znaleziony
+      res.status(404).json({ error: "Użytkownik nie znaleziony" });
+    }
+  } catch (error) {
+    console.error(error);
     res.status(500).json({ error: "Błąd serwera", details: error.message });
   }
 });
