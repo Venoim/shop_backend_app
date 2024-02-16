5e5a87aec43c2b6d11c1dbb3d237c105a4ce08da
Venoim <ascholz47@gmail.com>
2024-02-16 17:09:29 +0100

    Update

--- routes/orders.js
+++ routes/orders.js
@@ -81,8 +81,7 @@ router.get("/:userId", async (req, res) => {
 
 function generateRandomId(length) {
   let result = "";
-  const characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const characters = "0123456789";
   const charactersLength = characters.length;
   for (let i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
