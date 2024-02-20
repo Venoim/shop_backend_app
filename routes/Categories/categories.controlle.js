265b020f319c5ec13f1544086c9c3098eaefa14b
Venoim <ascholz47@gmail.com>
2024-02-20 11:17:40 +0100

    clear category file

--- routes/Categories/categories.controlle.js
+++ routes/Categories/categories.controlle.js
@@ -1,37 +1,18 @@
 import express from "express";
-import { connectToDB, connection } from "../../conectDB.js";
+import { getAllCategories } from "./categories.service.js";
 
 const router = express.Router();
 
-const from = "productCategories";
-
-await connectToDB();
-
-// Endpoint GET dla pobierania Kategorii
 router.get("/", async (req, res) => {
   try {
-    console.log("laczenie...");
-    const data = await getAll(from, connection);
-    console.log(data);
-    res.json(data);
+    const categories = await getAllCategories();
+    res.json(categories);
   } catch (error) {
     res.status(500).json({
-      error: "Błąd serwera",
+      error: "Internal Server Error",
       details: error.message,
     });
   }
 });
-
-async function getAll(from, connection) {
-  console.log("pobieram dane");
-  const sql = `SELECT * FROM public."${from}";`;
-  const res = await connection.query(sql);
-  console.log("ladowanie listy kategori");
-  const result = res.rows.map((item) => ({
-    id: item[0],
-    name: item[1],
-  }));
-  return result;
-}
 
 export default router;
---
+++ routes/Categories/categories.service.js
@@ -0,0 +1,16 @@
+import { connection } from "../../conectDB.js";
+
+const from = "productCategories";
+
+export const getAllCategories = async () => {
+  try {
+    const sql = `SELECT * FROM public."${from}";`;
+    const res = await connection.query(sql);
+    return res.rows.map((item) => ({
+      id: item[0],
+      name: item[1],
+    }));
+  } catch (error) {
+    throw new Error(`Error getting categories: ${error.message}`);
+  }
+};
